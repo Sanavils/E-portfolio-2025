3ca80d83b5ea49950f4643c07ab87c1b6e9a15a2
Sanavils <arkab94@gmail.com>
2025-06-11 20:45:33 +0200

    text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,13 @@ export default function Home() {
     return (
       <>
         <div className="fond-couleur"></div>
-      
+        <h1 className="Title">Hello<span className="CAPS-Small-text">[^-^]</span></h1>
+        <h2 className="H2">My name is HASSEN<br></br>And i’m a UI/UX DESIGNER</h2>
+       
+        <div >
+          <h2 className="H2">Everything starts with a line—the first mark, the origin of an idea. From this single path, concepts take shape, diverging into new directions before coming together in a unified vision.</h2>
+          <p className='overview'>Design is a continuous movement, a line unfolding into infinite possibilities. Each project begins with a singular idea, expanding into multiple directions, testing boundaries, and evolving through exploration. Yet, no matter how far the journey takes it, everything converges back into a seamless experience—clear, intentional, and impactful. My approach follows this principle: structured yet free, bold yet refined. Every interface, every interaction, every experience I create is a reflection of this philosophy.</p>
+        </div>
       
       </>
     )
