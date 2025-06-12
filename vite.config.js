ae7b01f9033435563be01be8ac1e8685ca1f1739
Sanavils <arkab94@gmail.com>
2025-06-12 14:54:41 +0200

    dist

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? './' : '/',
+  base:/E-portfolio-2025/,
   plugins: [react()],
   
 }))
