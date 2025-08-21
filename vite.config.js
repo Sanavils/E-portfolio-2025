e7d542226954898feb69d46f2f9be7620a707fab
Sanavils <arkab94@gmail.com>
2025-08-21 21:41:40 +0200

    changer le router

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig (({
-  base: /E-portfolio-2025/,
   plugins: [react()],
-  
+  base: /E-portfolio-2025/,
 }))
